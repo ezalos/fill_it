cadbba1cdb8f386cd7501e66e858b83a52cf7dea
ezalos <ezalos@gmail.com>
2018-11-26 22:56:46 +0100

    typos in recognize

--- srcs/recognize.c
+++ srcs/recognize.c
@@ -6,7 +6,7 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 21:41:20 by ldevelle          #+#    #+#             */
-/*   Updated: 2018/11/26 21:41:22 by ldevelle         ###   ########.fr       */
+/*   Updated: 2018/11/26 22:52:58 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static const char	part10(char *s, int i, int div, int mod)
 		{
 			if (s[i + (5 * 2) - 1] == '#' && div < 2)
 				return ("S1");
-			if (mod >= 3 && s[i + 5 - 2)] == '#')
+			if (mod >= 3 && s[i + 5 - 2] == '#')
 				return ("L3");
 			if (s[i + (5 * 2)] == '#' && div < 2)
 				return ("T1");
@@ -38,7 +38,7 @@ static const char	part1(char *s, int i, int div, int mod)
 				return ("J1");
 			if (s[i + (5 * 2) + 1] == '#' && div < 2)
 				return ("Z1");
-			if (s[i + (5 * 2) && div < 2] == '#'
+			if (s[i + (5 * 2) && div < 2] == '#')
 				return ("T3");
 		}
 		if (s[i + (5 * 2)] == '#' && div < 2)
@@ -81,7 +81,7 @@ static const char	part0(char *s, int i, int div, int mod)
 				return ("L2");
 		}
 	}
-	return (part1(s, i, div, mod););
+	return (part1(s, i, div, mod));
 }
 
 const char	recognize(char *s)
