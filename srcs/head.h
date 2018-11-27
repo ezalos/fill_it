6b1719cb84efc277fd139be6af6095c3386fb5e3
ezalos <ezalos@gmail.com>
2018-11-27 05:37:43 +0100

    Time taken by program calculated

--- srcs/head.h
+++ srcs/head.h
@@ -6,7 +6,7 @@
 /*   By: aboitier <aboitier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 16:30:33 by aboitier          #+#    #+#             */
-/*   Updated: 2018/11/27 02:19:55 by ldevelle         ###   ########.fr       */
+/*   Updated: 2018/11/27 05:36:36 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 char		*ft_read_check(char *fillit);
 const char	*recognize(char *s);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 02:13:04 by ldevelle          #+#    #+#             */
-/*   Updated: 2018/11/27 02:24:12 by ldevelle         ###   ########.fr       */
+/*   Updated: 2018/11/27 05:37:27 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 int		main(int ac, char **av)
 {
+	clock_t		t = clock();
+
+	double time_taken;
+	t = clock() - t;
 	ft_read_check(av[1]);
+	time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+	printf("It took %f seconds to execute \n", time_taken);
 	return (0);
 }
