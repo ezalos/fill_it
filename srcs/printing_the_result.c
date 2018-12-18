f3e24377da5ea6e18716a8bd4b148ec36209e647
Louis DEVELLE <ldevelle@e1r1p11.42.fr>
2018-12-18 19:14:44 +0100

    Color printing

--- srcs/printing_the_result.c
+++ srcs/printing_the_result.c
@@ -6,15 +6,41 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 17:18:38 by ldevelle          #+#    #+#             */
-/*   Updated: 2018/12/18 18:23:24 by ldevelle         ###   ########.fr       */
+/*   Updated: 2018/12/18 19:11:36 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
 
+void	ft_putchar_color(char c)
+{
+	int l;
+
+	l = (int)c - 65;
+	l = l % 6;
+	if (c == '.')
+		write(1, "\x1b[34m", 5);
+	else if (l == 0)
+		write(1, "\x1b[31m", 5);
+	else if (l == 1)
+		write(1, "\x1b[32m", 5);
+	else if (l == 2)
+		write(1, "\x1b[33m", 5);
+	else if (l == 3)
+		write(1, "\x1b[36m", 5);
+	else if (l == 4)
+		write(1, "\x1b[35m", 5);
+	else if (l == 5)
+		write(1, "\x1b[0m", 5);
+
+	write(1, &c, 1);
+	write(1, "\x1b[0m", 5);
+}
+
 void	print_pieces(t_head *head, char **tab_result)
 {
 	int i;
+	int j;
 
 //	printf("Print Pieces\n");
 	i = -1;
@@ -22,7 +48,10 @@ void	print_pieces(t_head *head, char **tab_result)
 	ft_putchar('\n');
 	while (++i < head->size_square)
 	{
-		ft_putstr(tab_result[i]);
+		j = -1;
+		ft_putstr("\t\t\t");
+		while (++j < head->size_square)
+			ft_putchar_color(tab_result[i][j]);
 		ft_putchar('\n');
 	}
 	ft_putchar('\n');
