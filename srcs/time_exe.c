cc95ea15b04f5363a061e042fbe66cf5000ee110
ezalos <ezalos@gmail.com>
2018-12-23 02:42:40 +0100

    time

--- srcs/time_exe.c
+++ srcs/time_exe.c
@@ -6,7 +6,7 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 12:52:19 by ldevelle          #+#    #+#             */
-/*   Updated: 2018/12/21 13:15:53 by ldevelle         ###   ########.fr       */
+/*   Updated: 2018/12/23 02:38:18 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,68 +16,95 @@
 time_exe(__func__, cl(clock()));
 print_time(time_exe(__func__, clock()));
 */
-
 void 	print_time_struct(t_time *g)
 {
-	printf(_RED"\tAdress:\t\t%p\n", (void *)g);
+	printf(_RED"\t\tAdress:\t\t%p\n", (void *)g);
 	printf(_CYAN "\t%s" _RESET, g->name);
 	ft_puttab(g->name, 4);
-	printf(_CYAN "Time: %Lf s.\n" _RESET, ((long double)g->t));
- 	printf("\tNEXT:\t\t%p\n" _RESET, (void*)g->next);
+	printf(_CYAN "Time: %f s.\n" _RESET, ((double)g->t));
+ 	printf("\t\tNEXT:\t\t%p\n\n" _RESET, (void*)g->next);
+}
+
+void	ft_puttab_nb(int length, int separation)
+{
+	int n = 4; //is equal to 8 for windows
+	int tab = 0;
+	while (tab + length < separation * n)
+	{
+		printf(" ");
+		tab++;
+	}
 }
 
 void	ft_puttab(const char *s, int separation)
 {
-	int tab;
+	int n = 4; //is equal to 8 for windows
+	int tab = 0;
+	int length = ft_strlen(s);
+	int bb = 0;
 
-	tab = ft_strlen(s);
-//	if (tab % 4 != 0)
-//		tab = (tab / 4) + 1;
-//	else
-		tab = tab / 4;
-	while (separation - tab++ >= 0)
-		printf("\t");
-//		write(1, "\t", 1);
+	while (tab + length < separation * n)
+	{
+		printf(" ");
+		tab++;
+	}
 }
 
 void	print_time(t_time *timee)
 {
-	t_time			*tmp;
-	long double		total;
-	int				tab;
+	t_time		*tmp;
+	double		total;
+	int			max_length = 0;
+	int			tab = 6;
 
 	total = 0;
 	tmp = timee;
-	while (tmp->next)
+	while (tmp)
 	{
-		total += (long double)tmp->t;
+		total += (double)tmp->t;
+		if (max_length < ft_strlen(tmp->name))
+			max_length = (ft_strlen(tmp->name) / 4);
 		tmp = tmp->next;
 	}
-	printf(_CYAN "Total time of program: %Lf\n" _RESET, (long double)total);
+	printf(_CYAN "\nTotal time of program: %f\n\n" _RESET, (double)total);
 
+	tab = max_length + 4;
 	tmp = timee;
-	while (tmp->next)
+	printf("\t________________________________________________________\n");
+	while (tmp)
 	{
-		printf(_CYAN "\t%Lf%%\tfor %s" _RESET, (((long double)tmp->t)/total) * 100, tmp->name);
-		ft_puttab(tmp->name, 4);
-		printf(_CYAN " in %Lf seconds\n" _RESET, ((long double)tmp->t));
+		if (total != 0)
+		{
+			printf("\t");
+				if ((int)(((tmp->t)/total) * 100) / 10 < 1)
+					printf(" ");
+			printf(_YELLOW "%d%%" _RESET, (int)(((tmp->t)/total) * 100));
+		}
+		else
+			printf("\t");
+		printf(_RED "\t%s" _RESET, tmp->name);
+		ft_puttab(tmp->name, tab);
+		printf(_MAGENTA "%f s\n" _RESET, ((double)tmp->t));
+		printf(_GREEN "\t%dn" _RESET, tmp->nb_call);
+		ft_puttab_nb(ft_intlen(tmp->nb_call), tab);
+		printf(_CYAN "Rt:\t%f s/n\n" _RESET, (double)(tmp->t/tmp->nb_call));
+		printf("\t________________________________________________________\n");
 		tmp = tmp->next;
 	}
 }
 
-long double	cl(clock_t t)
+double	cl(clock_t t)
 {
-	static long double	origin;
-	long double m;
+	static double	origin;
+	double m;
 
 	if (!origin)
-		origin = ((long double)(clock()));
-	m = ((long double)(t - origin))/CLOCKS_PER_SEC;
+		origin = ((double)(clock()));
+	m = ((double)(t - origin))/(CLOCKS_PER_SEC);
 	return (m);
-	//origin = ((long double)(clock()))/CLOCKS_PER_SEC;
 }
 
-static t_time	*time_link_creation(const char* s, long double t)
+static t_time	*time_link_creation(const char* s, double t)
 {
 	t_time	*timee;
 
@@ -85,12 +112,13 @@ static t_time	*time_link_creation(const char* s, long double t)
 		return (NULL);
 	timee->name = ft_strdup((const char*)s);
 	timee->t = t;
+	timee->nb_call = 1;
 	timee->next = NULL;
 //	print_time_struct(timee);
 	return(timee);
 }
 
-static t_time	*time_exceptions( t_time **timee, const char* s, long double t)
+static t_time	*time_exceptions( t_time **timee, const char* s, double t)
 {
 	if (s == NULL)
 		return (NULL);
@@ -101,17 +129,18 @@ static t_time	*time_exceptions( t_time **timee, const char* s, long double t)
 	}
 }
 
-static int		update_time( t_time *timee, const char* s, long double t)
+static int		update_time( t_time *timee, const char* s, double t)
 {
-	if (ft_strstr(s, timee->name))
+	if (!ft_strcmp(s, timee->name))
 	{
 		timee->t = timee->t + t;
+		timee->nb_call++;
 		return (1);
 	}
 	return (0);
 }
 
-t_time	*time_exe(const char* s, long double t)
+t_time	*time_exe(const char* s, double t)
 {
 	static t_time		*timee;
 	t_time				*tmp;
@@ -123,12 +152,12 @@ t_time	*time_exe(const char* s, long double t)
 	tmp = timee;
 	while (tmp->next != NULL)
 	{
-//		printf(_RESET"WHILE\n"_CYAN);
-//		print_time_struct(tmp);
 		if (update_time(tmp, s, t))
 			return (timee);
 		tmp = tmp->next;
 	}
+	if (update_time(tmp, s, t))
+		return (timee);
 	tmp->next = time_link_creation(s, t);
 	return (timee);
 }
--- srcs/time_exe.h
+++ srcs/time_exe.h
@@ -6,44 +6,64 @@
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 12:51:05 by ldevelle          #+#    #+#             */
-/*   Updated: 2018/12/21 13:01:37 by ldevelle         ###   ########.fr       */
+/*   Updated: 2018/12/23 02:41:08 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TIME_EXE_H
 # define TIME_EXE_H
 
-# include "head.h"
+# define WINDOWS
 
+# include "libft.h"
 # include <time.h>
 # include <stdlib.h>
 # include <stdio.h>
 
-# define _RED     "\x1b[31m"
-# define _GREEN   "\x1b[32m"
-# define _YELLOW  "\x1b[33m"
-# define _BLUE    "\x1b[34m"
-# define _MAGENTA "\x1b[35m"
-# define _CYAN    "\x1b[36m"
-# define _RESET   "\x1b[0m"
+# ifdef WINDOWS
+#  define _RED     ""
+#  define _GREEN   ""
+#  define _YELLOW  ""
+#  define _BLUE    ""
+#  define _MAGENTA ""
+#  define _CYAN    ""
+#  define _RESET   ""
+# endif
+
+# ifdef MAC
+#  define _RED     "\x1b[31m"
+#  define _GREEN   "\x1b[32m"
+#  define _YELLOW  "\x1b[33m"
+#  define _BLUE    "\x1b[34m"
+#  define _MAGENTA "\x1b[35m"
+#  define _CYAN    "\x1b[36m"
+#  define _RESET   "\x1b[0m"
+# endif
 
 typedef struct			s_time
 {
-	long double			t;
-	long double			percent;
+	double				t;
+	int					nb_call;
 	char				*name;
 	struct	s_time		*next;
 }						t_time;
 
 //main
-t_time	*time_exe(const char* s, long double t);
-long double	cl(clock_t t);
+t_time	*time_exe(const char* s, double t);
+double	cl(clock_t t);
 void	print_time(t_time *time);
 
 //others
-static t_time	*time_link_creation(const char* s, long double t);
+static t_time	*time_link_creation(const char* s, double t);
 void			ft_puttab(const char *s, int separation);
-//char	*ft_strdup(const char *s);
-//char	*ft_strstr(const char *meule_de_foin, const char *aiguille);
+
+
+//libft
+size_t		ft_strlen(const char *s);
+char		*ft_strstr(const char *meule_de_foin, const char *aiguille);
+char		*ft_strdup(const char *src);
+int			ft_intlen(int n);
+int			ft_strcmp(const char *s1, const char *s2);
+
 
 #endif
