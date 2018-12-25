04f13371ba810d20c1e9a921762b8d35702b3fe0
ezalos <ezalos@gmail.com>
2018-12-25 10:20:53 +0100

    binary gestion : 60 %

--- srcs/binary.c
+++ srcs/binary.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   setup.c                                            :+:      :+:    :+:   */
+/*   binary.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldevelle <ldevelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/29 04:53:29 by ldevelle          #+#    #+#             */
-/*   Updated: 2018/12/25 10:07:32 by ldevelle         ###   ########.fr       */
+/*   Created: 2018/12/25 10:07:38 by ldevelle          #+#    #+#             */
+/*   Updated: 2018/12/25 10:19:35 by ldevelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "head.h"
 
-void	write_solutions(t_head *head)
+void	write_binary(t_head *head)
 {
 	t_piece *piece;
 	int	i;
@@ -62,49 +63,52 @@ void	write_solutions(t_head *head)
 	}
 	piece = piece->next;
 	current_piece++;
-
-
 }
 
-void	setup_pieces(t_head *head)
+int	ft_iterative_power(int nb, int power)
 {
-	t_piece		*tmp;
-	int			i;
+	int x;
 
-	i = 0;
-	while (++i <= head->p)
+	x = 1;
+	if (power == 0)
+		return (1);
+	if (power < 0)
+		return (0);
+	while (power > 0)
 	{
-		//printf("WESH\n");
-		pieces_yx(find_piece(head, i));
-		find_piece(head, i)->pc_pos = size_pieces(find_piece(head, i)->name[0], head->size_square);
-//		piece_placement(tmp); not sure about the purpose of this function
-		if (i == 1)
-			find_piece(head, i)->tt_pos = find_piece(head, i)->pc_pos;
-		else
-			find_piece(head, i)->tt_pos = find_piece(head, i)->pc_pos + find_piece(head, i - 1)->tt_pos;//		sum_placement(tmp);
-		//printf("HELLO\n");
-		coord_setup(find_piece(head, i));
-		head->tt_pos_all = find_piece(head, i)->tt_pos;
-		find_piece(head, i)->i = i;
-		//printf("\nPIECE : %d\npc_pos %d\ntt_pos %d\ntt_pos_all%d\n", i, find_piece(head, i)->pc_pos, find_piece(head, i)->tt_pos, head->tt_pos_all);
+		x = x * nb;
+		power--;
 	}
+	return (x);
 }
 
-char	**malloc_solution(t_head *head)
+void	binary_to_str(char *binary, int binary_position)
 {
+	binary[binary_position / 8] += ft_iterative_power(2, binary_position % 8);
+}
+
+int		binary_size(int length)
+{
+	if (length % 8 != 0)
+		return ((length / 8) + 1 + 1);
+	return ((length / 8) + 1);
+}
+
+char	**malloc_binary(t_head *head)
+{
+
 	char	**sol;
 	int		i;
 	int		line;
 	int		u;
 
 	//printf("1\n%d\n", head->tt_pos_all);
-	if (!(sol = (char**)malloc(sizeof(char*) * (head->tt_pos_all))))
+	if (!(sol = (char**)malloc(sizeof(char*) * head->tt_pos_all)))
 		return (NULL); //need to protect if malloc has a pbm during allocation
 	i = -1;
-	line = head->p + (head->size_square * head->size_square) + 1;
+	line = binary_size(head->p + (head->size_square * head->size_square));
 	while (++i < head->tt_pos_all)
 	{
-		//printf("%d\n", i);
 		if (!(sol[i] = (char*)malloc(sizeof(char) * line)))
 			return (NULL); //need to protect if malloc has a pbm during allocation
 		u = -1;
@@ -112,37 +116,4 @@ char	**malloc_solution(t_head *head)
 			sol[i][u] = 0;
 	}
 	return (sol);
-}
-
-t_head	*setup_head_sol_part(t_head *head)
-{
-	int i;
-
-	if (!(head->solution = malloc_solution(head)))
-		return (NULL);
-	if (!(head->y_all_PxNx = (char*)malloc(sizeof(char) * (head->tt_pos_all))))
-		return (NULL);
-	i = -1;
-	while (++i < head->tt_pos_all + 1)
-		head->y_all_PxNx[i] = 1;
-	//	head->sol->possible_solutions = 0;
-	head->the_choosen_configuration = 1;
-	write_solutions(head);
-	i = 0;
-	while (i <= head->p)// one less might be better
-		if (!(next_solve_step(head, i++)))
-			return (NULL);
-	return (head);
-}
-
-t_head	*setup_head(t_head *head)
-{
-	//printf("Nb of pieces = %d\n", head->p);
-	head->size_square = (float_to_int(f_sqrt(head->p, 0) * 2));
-	//printf("size_square = %d\n", head->size_square);
-	setup_pieces(head);
-	if (!(setup_head_sol_part(head)))
-		return (NULL);
-//	time_exe(__func__, clock());
-	return (head);
 }
